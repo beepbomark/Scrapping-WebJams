dc180469d519752c207f69b62aa021dc356ef0d1
Liu Hanlong <68144485+beepbomark@users.noreply.github.com>
2023-05-02 19:58:52 +0800

    update

--- moneh.py
+++ moneh.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 def fetch_jams(url):
     try:
@@ -12,13 +13,10 @@ def fetch_jams(url):
 
     return None
 
-def find_dollar_sign(text):
-    return "$" in text
-
 if __name__ == '__main__':
     url = "https://itch.io/jams"
     html_content = fetch_jams(url)
-    
+
     if html_content:
         soup = BeautifulSoup(html_content, 'html.parser')
         jam_cells = soup.find_all('div', class_='jam_cell')
@@ -30,38 +28,28 @@ if __name__ == '__main__':
             jam_link_element = jam_cell.find('a')
             jam_link = 'https://itch.io' + jam_link_element.get('href')
             jam_title = jam_link_element.text.strip()
-            
-            joined_count_element = jam_cell.find('span', class_='joined_count')
-            if joined_count_element:
-                count_text = joined_count_element.text.strip('()').split()[0]
-                count_text_no_commas = count_text.replace(',', '')
-                jam_joined = int(count_text_no_commas)
-            else:
-                jam_joined = 0
 
-            # Fetch the jam page content
             jam_page_content = fetch_jams(jam_link)
-            dollar_sentences = []
 
             if jam_page_content:
                 jam_page_soup = BeautifulSoup(jam_page_content, 'html.parser')
-                paragraphs = jam_page_soup.find_all('p')
+                description = jam_page_soup.find('div', class_='jam_content')
 
-                for paragraph in paragraphs:
-                    if find_dollar_sign(paragraph.text):
-                        dollar_sentences.append(paragraph.text.strip())
+                if description and re.search(r'\$', description.text):
+                    joined_count_element = jam_cell.find('span', class_='joined_count')
+                    if joined_count_element:
+                        count_text = joined_count_element.text.strip('()').split()[0]
+                        count_text_no_commas = count_text.replace(',', '')
+                        jam_joined = int(count_text_no_commas)
+                    else:
+                        jam_joined = 0
 
-            if dollar_sentences:  # Only add jam data if dollar_sentences is not empty
-                jam_data.append({
-                    'Jam ID': jam_id,
-                    'Jam Link': jam_link,
-                    'Jam Title': jam_title,
-                    'Joined Count': jam_joined,
-                    'Dollar Sentences': ' | '.join(dollar_sentences)
-                })
+                    jam_data.append([jam_id, jam_link, jam_title, jam_joined, description.text.strip()])
 
-        df = pd.DataFrame(jam_data)
-        df.to_csv('jam_data_with_dollar_signs.csv', index=False)
+        df = pd.DataFrame(jam_data, columns=['Jam ID', 'Jam Link', 'Jam Title', 'Joined Count', 'Description'])
+        
+        # Uncomment the next line to export the DataFrame to a CSV file
+        df.to_csv('jam_data.csv', index=False)
 
         print(df)
     else:
