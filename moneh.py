ef4bf1a17ab2136b6d13bbc5ab29dfa279ba0352
Liu Hanlong <68144485+beepbomark@users.noreply.github.com>
2023-05-11 23:18:59 +0800

    Update moneh.py

--- moneh.py
+++ moneh.py
@@ -25,6 +25,9 @@ def write_to_sheet(df):
     
     # Convert all data in DataFrame to string
     df = df.astype(str)
+    
+    # Convert DataFrame to JSON and back to ensure all types are JSON serializable
+    df = pd.read_json(df.to_json())
 
     # Write DataFrame to Google Sheet
     for i in range(len(df)):
